4f8b38040f9b788f9c404e0947e974444014173a
maansaab95170 <maansaab95170@gmail.com>
2023-03-01 01:19:42 +0530

    Changes in HTML and CSS
    I am mostly done with the design, there might be some minor changes in future, but nothing major. It is not resposive yet so that will be taken care of in next commit, along with JS

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
-function init() {
-    var ofvalue = document.getElementById('ofbox');
-    var a = 1;
-    ofvalue.innerText = "0" + a + " of 03";
-}
-
-document.addEventListener('DOMContentLoaded', init, false);
-
-
 function changeImage1() {
-    var image = document.getElementById('myImage');
-    var ofvalue = document.getElementById('ofbox');
-    var a;
-    if (image.src.match("Fashion3.jpg")) {
-        image.src = "Fashion2.jpg";
-        a = 2;
-    } else {
-        image.src = "Fashion.jpg";
-        a = 1;
-
-    }
-    ofvalue.innerHTML = "0" + a + " of 03";
-
-
+  var image = document.getElementById("myImage");
+  var ofvalue = document.getElementById("ofbox");
+  var a;
+  if (image.src.match("Images/Fashion3.jpg")) {
+    image.src = "Images/Fashion2.jpg";
+    a = 2;
+  } else {
+    image.src = "Images/Fashion.jpg";
+    a = 1;
+  }
+  ofvalue.innerHTML = "0" + a + " of 03";
 }
 
 function changeImage2() {
-    var image = document.getElementById('myImage');
-    var ofvalue = document.getElementById('ofbox');
-    var a;
-    if (image.src.match("Fashion.jpg")) {
-        image.src = "Fashion2.jpg";
-        a = 2;
-    } else {
-        image.src = "Fashion3.jpg";
-        a = 3;
-    }
-    ofvalue.innerHTML = "0" + a + " of 03";
-
+  var image = document.getElementById("myImage");
+  var ofvalue = document.getElementById("ofbox");
+  var a;
+  if (image.src.match("Images/Fashion.jpg")) {
+    image.src = "Images/Fashion2.jpg";
+    a = 2;
+  } else {
+    image.src = "Images/Fashion3.jpg";
+    a = 3;
+  }
+  ofvalue.innerHTML = "0" + a + " of 03";
 }
 
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more <span id='leftarrow'><i class='fa fa-caret-right' aria-hidden='true'></i></span>";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "<span id='leftarrow'><i class='fa fa-caret-left' aria-hidden='true'></i></span> Read less";
-        moreText.style.display = "inline";
-    }
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more <span id='leftarrow'><i class='fa fa-caret-right' aria-hidden='true'></i></span>";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "<span id='leftarrow'><i class='fa fa-caret-left' aria-hidden='true'></i></span> Read less";
+    moreText.style.display = "inline";
+  }
 }
