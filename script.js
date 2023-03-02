541243a2d927bb53883a431e451a722f1eda2748
maansaab95170 <maansaab95170@gmail.com>
2023-03-02 23:41:41 +0530

    Added JS and made the page somewhat responsive

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-function changeImage1() {
-  var image = document.getElementById("myImage");
-  var ofvalue = document.getElementById("ofbox");
-  var a;
-  if (image.src.match("Images/Fashion3.jpg")) {
-    image.src = "Images/Fashion2.jpg";
-    a = 2;
-  } else {
-    image.src = "Images/Fashion.jpg";
-    a = 1;
-  }
+let images = ["Images/Fashion1.jpg", "Images/Fashion2.jpg", "Images/Fashion3.jpg", "Images/Fashion4.jpg", "Images/Fashion5.jpg"];
+const image = document.getElementById("slideImage");
+const previous = document.getElementById("previousImg");
+const next = document.getElementById("nextImg");
+
+let currentImg = 0;
+
+function previousImage() {
+  if (currentImg > 0) currentImg--;
+  image.src = images[currentImg];
+}
+function nextImage() {
+  if (currentImg < images.length - 1) currentImg++;
+  image.src = images[currentImg];
 }
 
-function changeImage2() {
-  var image = document.getElementById("myImage");
-  var ofvalue = document.getElementById("ofbox");
-  var a;
-  if (image.src.match("Images/Fashion.jpg")) {
-    image.src = "Images/Fashion2.jpg";
-    a = 2;
-  } else {
-    image.src = "Images/Fashion3.jpg";
-    a = 3;
-  }
-}
+// function ReadMoreLess() {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("myBtn");
 
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more <span id='leftarrow'><i class='fa fa-caret-right' aria-hidden='true'></i></span>";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "<span id='leftarrow'><i class='fa fa-caret-left' aria-hidden='true'></i></span> Read less";
-    moreText.style.display = "inline";
-  }
-}
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more <span id='leftarrow'><i class='fa fa-caret-right' aria-hidden='true'></i></span>";
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "<span id='leftarrow'><i class='fa fa-caret-left' aria-hidden='true'></i></span> Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
